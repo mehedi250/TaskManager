72e473f991c18538e4bbb66b91cc2d5f891062c6
mehedihasan250 <mehedi@orangetoolz.com>
2022-07-27 00:35:02 +0600

    optimize code

--- resources/js/components/pages/project/ProjectCreate.js
+++ resources/js/components/pages/project/ProjectCreate.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react'
-import { Button, Card, Form } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
+import { Button, Form, Modal, Spinner } from 'react-bootstrap'
 import Swal from "sweetalert2"; 
 import {NotificationManager} from 'react-notifications';
 import { createProjectApi } from '../../../api/serviceApi';
 
 class ProjectCreate extends Component {
-    state = {
-        isLoading: false,
-        name: "",
-        description: "",
-        errors: []
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoading: false,
+            name: "",
+            description: "",
+            show: true,
+            errors: []
+        };
+    }
 
     handleName =(e)=>{
         this.setState({name: e.target.value});
@@ -30,6 +32,8 @@ class ProjectCreate extends Component {
             if(response.data.success){
                 Swal.fire({icon: 'success', title: response.data.message, showConfirmButton: false, timer: 1500})
                 this.setState({name: "", description: "", errors: []});
+                this.handleClose();
+                this.props.handleProjectComplete(response.data.data);
             }
             else{
                 if(response.data.status === 'validation-error'){
@@ -50,10 +54,50 @@ class ProjectCreate extends Component {
     handleError = (key)=>{
         return (this.state.errors[key] != undefined)?this.state.errors[key]:'';
     }
+
+    handleClose = () =>{
+        this.props.handleCreateProject(false);
+        this.setState({show: false});
+    }
     render() {
         return (
         <>
-            <div className="header-part">
+        <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header>
+                <Modal.Title>Create New Project
+                    <div></div>
+                </Modal.Title>
+                <button className='btn btn-outline-info btn-sm' onClick={this.handleClose}>x</button>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Project Name <span className="text-danger">*</span></Form.Label>
+                        <Form.Control onChange={this.handleName} value={this.state.name} type="text" placeholder="Enter Project Name" />
+                        <Form.Text className="text-danger">{this.handleError('name')}</Form.Text>
+                    </Form.Group>
+                    
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Description <span className="text-danger">*</span></Form.Label>
+                        <Form.Control onChange={this.handleDescription} value={this.state.description} type="text" as="textarea" rows="3" placeholder="Enter Project Description" />
+                        <Form.Text className="text-danger">{this.handleError('description')}</Form.Text>
+                    </Form.Group>
+                    
+                    { (this.state.isLoading)?
+                    <Button variant="primary" type="button" disabled>
+                        <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+                        Saving...
+                    </Button>
+                    :
+                    <Button variant="primary" type="submit">
+                        Add Project
+                    </Button>
+                    }
+                    
+                </Form>
+            </Modal.Body>
+        </Modal>
+            {/* <div className="header-part">
                 <div className="float-left">
                     <h2>Create New Project </h2>
                 </div>
@@ -91,7 +135,7 @@ class ProjectCreate extends Component {
                         
                     </Form>
                 </Card.Body>
-            </Card>
+            </Card> */}
         </>
         )
     }
--- resources/js/components/pages/project/ProjectList.js
+++ resources/js/components/pages/project/ProjectList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Badge, Button, Card, Spinner } from 'react-bootstrap';
+import { Badge, Button, Card, Form, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Swal from "sweetalert2"; 
@@ -7,13 +7,21 @@ import { deleteProjectApi, getProjectListApi } from '../../../api/serviceApi';
 
 import { faTrash, faCheckDouble } from "@fortawesome/free-solid-svg-icons";
 import { NotificationManager } from 'react-notifications';
+import ProjectCreate from './ProjectCreate';
 
 
 export default class ProjectList extends Component {
-    state = {
-        isLoading: true,
-        projectList: []
-    };
+    constructor(props){
+        super(props);
+        this.timeout =  0;
+        this.state = {
+            isLoading: true,
+            isCreateProject: false,
+            search: "",
+            projectList: []
+        };    
+    }
+    
 
     componentDidMount(){
         this.getProjectList();
@@ -28,6 +36,41 @@ export default class ProjectList extends Component {
             this.setState({projectList: [], isLoading: false});
             console.log("LandingPop", error)
         });
+    }
+
+    renderProjectList(){
+        let view = [];
+        this.state.projectList.map((project, index)=>{
+            view.push(
+                <div className="col-md-6 py-2" key={index}>
+                    <Card>
+                        <Card.Header>
+                            {project.name} <Badge className='text-white' variant="primary">{project.tasks_count}</Badge>
+                            {(project.status===1) &&  <span className='float-right text-success'><FontAwesomeIcon  icon={faCheckDouble} /></span>}
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                {project.description}
+                            </Card.Text>
+                            <Link to={`/projects/${project.id}`}>
+                                <Button variant='primary' className='mr-2 btn-sm'>View & Edit</Button>
+                            </Link>
+                            <Button variant='danger' onClick={()=>this.handleDelete(project.id, index)} className='btn-sm'><FontAwesomeIcon  icon={faTrash} /> Delete</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+            )
+        })
+        if(view.length === 0){
+            return(
+                <div className='col-md-7 mx-auto p-4'>
+                    <div className="text-center p-4 ">
+                        No project created yet ! 
+                    </div>
+                </div>
+            )
+        }
+        return view;
     }
 
     removeListItem =(removeIndex)=>{
@@ -52,26 +95,54 @@ export default class ProjectList extends Component {
     }
 
     handleDelete=(id, removeIndex)=>{
-        if(this.deletetItemConformtion()){
-            this.setState({isLoading: true});
-            deleteProjectApi(id).then((response) => {
-                if(response.data.success){
-                    Swal.fire({icon: 'success', title: response.data.message, showConfirmButton: false, timer: 1500})
-                    const newProjectList = this.removeListItem(removeIndex);
-                    this.setState({projectList: newProjectList});
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                this.setState({isLoading: true});
+                deleteProjectApi(id).then((response) => {
+                    if(response.data.success){
+                        Swal.fire({icon: 'success', title: response.data.message, showConfirmButton: false, timer: 1500})
+                        const newProjectList = this.removeListItem(removeIndex);
+                        this.setState({projectList: newProjectList});
+                        this.setState({isLoading: false});
+                    }else{
+                        this.setState({isLoading: false});
+                        NotificationManager.error(response.data.message, 'Error!');
+                    }
+                    
+                })
+                .catch(error=>{
                     this.setState({isLoading: false});
-                }else{
-                    this.setState({isLoading: false});
-                    NotificationManager.error(response.data.message, 'Error!');
-                }
-                
-            })
-            .catch(error=>{
-                this.setState({isLoading: false});
-                console.log("LandingPop", error)
-            });    
-        }
+                    console.log("LandingPop", error)
+                }); 
+            }
+        })
     }
+
+    handleCreateProject=(value=true)=>{
+        this.setState({isCreateProject: value});
+    }
+
+    handleProjectComplete=(data)=>{
+        this.setState({projectList: [data, ...this.state.projectList]});
+    }
+
+    handleSearch=(e)=>{
+        const search = e.target.value;
+        this.setState({search: search})
+        if(this.timeout) clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => {
+          console.log(search)
+        }, 800);
+    }
+
     render() {
         return (
             <>
@@ -79,8 +150,12 @@ export default class ProjectList extends Component {
                 <div className="float-left">
                     <h2>Projects <Badge className='text-white' variant="primary">{this.state.projectList.length}</Badge></h2>
                 </div>
-                <div className="float-right">
-                    <Link className="btn btn-info text-white" to="/projects/create">+ Create New</Link>
+                <div className="float-right d-flex">
+                    <Form.Control  onChange={this.handleSearch} value={this.state.search} type="text" placeholder="Search Project" />
+                    <div className='text-right' style={{width: "210px"}}>
+                        <button style={{display:"inline"}} className="btn btn-info text-white" onClick={this.handleCreateProject}>+ Create New</button>
+                    </div>
+                    
                 </div>
             </div>
             <div className="clearfix"></div>
@@ -95,27 +170,12 @@ export default class ProjectList extends Component {
             </div>
             }
 
-            {!this.state.isLoading && 
-            this.state.projectList.map((project, index)=>(
-                <div className="col-md-6 py-2" key={index}>
-                    <Card>
-                        <Card.Header>
-                            {project.name} <Badge className='text-white' variant="primary">{project.tasks_count}</Badge>
-                            {(project.status===1) &&  <span className='float-right text-success'><FontAwesomeIcon  icon={faCheckDouble} /></span>}
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Text>
-                                {project.description}
-                            </Card.Text>
-                            <Link to={`/projects/${project.id}`}>
-                                <Button variant='primary' className='mr-2 btn-sm'>View & Edit</Button>
-                            </Link>
-                            <Button variant='danger' onClick={()=>this.handleDelete(project.id, index)} className='btn-sm'><FontAwesomeIcon  icon={faTrash} /> Delete</Button>
-                        </Card.Body>
-                    </Card>
-                </div>
-            ))}
+            {!this.state.isLoading &&  this.renderProjectList() }    
             </div>
+
+            {this.state.isCreateProject && 
+                <ProjectCreate  handleCreateProject={this.handleCreateProject} handleProjectComplete={this.handleProjectComplete} />
+            }
             </>
         )
     }
